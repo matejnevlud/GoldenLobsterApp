effab86ef6ef0890ecd47ff0ad5b9c194141b949
Matěj Nevlud <matej.nevlud@bico.digital>
2024-07-22 16:25:58 +0200

    fix

--- App.tsx
+++ App.tsx
@@ -1,118 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect } from "react";
+import { WebView } from "react-native-webview";
+import { Button, StatusBar, StyleSheet, TextInput, View } from "react-native";
+import SystemNavigationBar from "react-native-system-navigation-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    const [textInput, setTextInput] = React.useState("");
+    const [webUrl, setWebUrl] = React.useState(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    useEffect(() => {
+        AsyncStorage.getItem("textInput").then((value) => {
+            console.log("value", value);
+            if (value) {
+                setTextInput(value);
+            }
+        }).catch(console.error);
+    }, []);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    useEffect(() => {
+        StatusBar.setHidden(true);
+        SystemNavigationBar.navigationHide(true);
+        SystemNavigationBar.stickyImmersive(true);
+    }, [webUrl, textInput]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    const onPress = async () => {
+        console.log("onPress");
+        const res = await AsyncStorage.setItem("textInput", textInput);
+        setWebUrl(textInput);
+    };
+
+    if (!webUrl)
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <TextInput style={{ height: 60, width: "90%", backgroundColor: "#eee", marginBottom: 20, fontSize: 20 }} placeholder="   Enter url e.g. 10.0.0.27:3000" value={textInput} onChangeText={setTextInput} />
+                <Button title="Connect To Server" onPress={() => onPress()} />
+            </View>
+        );
+
+
+    return (
+        <WebView
+            style={styles.container}
+            source={{ uri: webUrl + "?t=" + Date.now() }}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    container: {
+        flex: 1
+    }
 });
-
-export default App;
